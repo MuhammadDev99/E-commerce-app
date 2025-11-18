b12d64ad58da325272f04343af364a239bcd707d
MuhammadDev99 <mohammad.onthefloor@gmail.com>
2025-11-18 23:14:55 +0300

    update

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-const API_BASE = "http://localhost:3333"
+/* const API_BASE = "http://localhost:3333" */
+const API_BASE = "https://muhammad.developer.li:3333"
 const SHOP_NAME = "SparkyShop"
 const LOCAL_STORAGE_USER_KEY = "user"
 export { API_BASE, SHOP_NAME, LOCAL_STORAGE_USER_KEY }
